36011b9ef77e9579c80a5d02906a3bd67e9a7c8f
AlexCrownshaw <you@example.com>
2020-09-20 21:21:20 +0100

    Reformmated to use multiple functions (UDL not working)

--- ShearForceBendingMomentsCalculator.cpp
+++ ShearForceBendingMomentsCalculator.cpp
@@ -4,36 +4,95 @@
 
 using namespace std;
 
-bool SortDistance (const vector <double>& v1, const vector <double>& v2)    {
-    return v1.at(1) < v2.at(1);
-}
+//Function Prototypes
+double inputBeamLength ();
+vector <vector<double>> inputPointForces(vector <double>, double);
+vector <vector<double>> inputUDL(vector <double>, double);
+vector <double> inputSupports(double);
+vector <double> UDL2Point(vector <vector<double>>, vector <double>, int);
+double computeSum (vector <vector<double>>);
+double computeR_b (vector <vector<double>>, vector <double>);
+double computeR_a (double, double);
+void outputReactions(double, double, double);
+vector <vector<double>> createForceVector (vector <vector<double>>, vector <vector<double>>, vector <double>, vector <double>, double, double);
+bool SortDistance (const vector <double>&, const vector <double>&);
+void outputForceVector (vector <vector<double>>);
+double computeStepSize (double);
+double computeVx (vector <vector<double>>, double, int);
+double computeMx (vector <vector<double>>, double, int);
+void outputSFBM (vector <vector<double>>);
 
 int main()  {
+    double beamLength {inputBeamLength()};
+    vector <double> vec {0, 0, 0};
+    vector <vector<double>> pointForceVector_2d {inputPointForces(vec, beamLength)};
+    vector <vector<double>> UDLVector_2d {inputUDL(vec, beamLength)};
+    vector <double> supportDistances {inputSupports(beamLength)};
+
+    for (int i {}; i < UDLVector_2d.size(); i++)    {
+        pointForceVector_2d.push_back(UDL2Point(UDLVector_2d, vec, i));
+    }
+    double sumPointForces {computeSum(pointForceVector_2d)};
+    double R_b {computeR_b(pointForceVector_2d, supportDistances)};
+    double R_a {computeR_a(R_b, sumPointForces)};
+    outputReactions(sumPointForces, R_a, R_b);
+
+    vector <vector<double>> forceVector {createForceVector(pointForceVector_2d, UDLVector_2d, supportDistances, vec, R_a, R_b)};
+    sort(forceVector.begin(), forceVector.end(), SortDistance);
+    outputForceVector(forceVector);
+
+    double stepSize {computeStepSize(beamLength)};
+    double Vx {};
+    double Mx {};
+    vector <vector<double>> SFBM_2D {};
+    for (double x {}; x <= beamLength; x += stepSize)   {
+        for (int i {}; i < forceVector.size(); i++)   {
+            if (x >= forceVector.at(i).at(1))   {
+                Vx += computeVx(forceVector, x, i);
+                Mx += computeMx(forceVector, x, i);
+            }
+        }
+        vec.at(0) = x;
+        vec.at(1) = Vx;
+        vec.at(2) = Mx;
+        SFBM_2D.push_back(vec);
+        Vx = 0;
+        Mx = 0;
+    }
+    outputSFBM(SFBM_2D);
+}
+
+double inputBeamLength()    {
     cout << "Enter the length of the beam in metres: ";
     double beamLength {};
     cin >> beamLength;
+    return beamLength;
+}
 
-    cout << "Enter the number of point forces: ";
+vector <vector<double>> inputPointForces(vector <double> vec, double beamLength) {
+        cout << "Enter the number of point forces: ";
     int numberPointForces {};
     cin >> numberPointForces;
     cout << "Enter the magnitude of each force followed by the distance in metres from the left end of the beam: ";
-    vector <vector<double>> pointForceVector_2d {};                 
-    vector <double> vec {0, 0, 0};                           /*Uses a 1D vector to intiliase a point force and a distance from the left hand side of the beam.*/ 
-    for (size_t i {}; i < numberPointForces; i++) {          /*Iterates until all point forces specified by the user are inputted.*/       
-        cin >> vec.at(0) >> vec.at(1);
-        vec.at(2) = 0;                                        /*1D vector is pushed back into 2D vector which comprises of all the point forces and distances.*/
-        if (vec.at(1) <= beamLength)   {                     /*If statement checks that forces are within the user specified beam length*/
+    vector <vector<double>> pointForceVector_2d {};                                                                          
+    for (size_t i {}; i < numberPointForces; i++) {      
+        cin >> vec.at(0) >> vec.at(1);                       
+        vec.at(2) = 0;                                       
+        if (vec.at(1) <= beamLength)   {                    
             pointForceVector_2d.push_back(vec);
             cout << pointForceVector_2d.at(i).at(0) << " Newtons " << pointForceVector_2d.at(i).at(1) << " metres from the left" << endl;
         }
         else    {
             cout << "The distance of a point forces must be less than the length of the beam:" << endl;
+            i--;
         }
     }
-    cout << "Number of point forces = " << (pointForceVector_2d.size())<< endl;
-
+    cout << "Number of point forces = " << pointForceVector_2d.size() << endl;
     cout << "================================================" << endl;
+    return pointForceVector_2d;
+}
 
+vector <vector<double>> inputUDL(vector <double> vec, double beamLength)  {
     cout << "Enter the number of UDL's: ";
     int numberUDL {};
     cin >> numberUDL;
@@ -48,60 +107,81 @@ int main()  {
             }
             else    {
                 cout << "The start distance must be less than the end distance." << endl;
+                i--;
             }
         }
         else    {
             cout << "The UDL must be within the beam length." << endl;
+            i--;
         }
     }
+    cout << "Number of UDL's = " << UDLVector_2d.size() << endl;
     cout << "================================================" << endl;
+    return UDLVector_2d;
+}
 
-    cout << "Enter the distance of both supports from the left side: " ;
-    vector <double> SupportDistances {};
-    double supportUserInput {};                                     /*User inputs the position of two supports which are stored in another 1D Vector.*/
-    for (size_t i {}; i < 2; i++)    {                              /*The use of a vector in this case allows for future versions to compute beams with more than 2 supports*/
+vector <double> inputSupports(double beamLength)    {
+    cout << "Enter the distance of both supports from the left side: ";
+    vector <double> supportDistances {};
+    double supportUserInput {};                                     
+    for (size_t i {}; i < 2; i++)    {                              
         cin >> supportUserInput;
         if (supportUserInput <= beamLength) {
-            SupportDistances.push_back(supportUserInput);
-            cout << "support " << SupportDistances.at(i) << " metres from the left" << endl;
+            supportDistances.push_back(supportUserInput);
+            cout << "support " << supportDistances.at(i) << " metres from the left" << endl;
         }
         else    {
             cout << "The distance of a support must be less than the length of the beam:" << endl;
+            i--;
         }
     }
-    cout << "Number of supports = " << SupportDistances.size() << endl;
-
+    cout << "Number of supports = " << supportDistances.size() << endl;
     cout << "================================================" << endl;
+    return supportDistances;
+}
 
-    for (int i {}; i < UDLVector_2d.size(); i++)    {
-        vec.at(0) = UDLVector_2d.at(i).at(0) * (UDLVector_2d.at(i).at(2) - UDLVector_2d.at(i).at(1));
-        vec.at(1) = UDLVector_2d.at(i).at(1) + ((UDLVector_2d.at(i).at(2) - UDLVector_2d.at(i).at(1)) / 2);
-        vec.at(2) = 1;
-        pointForceVector_2d.push_back(vec);
-    }
+vector <double> UDL2Point(vector <vector<double>> UDLVector_2d, vector <double> vec, int i) {
+    vec.at(0) = UDLVector_2d.at(i).at(0) * (UDLVector_2d.at(i).at(2) - UDLVector_2d.at(i).at(1));
+    vec.at(1) = UDLVector_2d.at(i).at(1) + ((UDLVector_2d.at(i).at(2) - UDLVector_2d.at(i).at(1)) / 2);
+    vec.at(2) = 1;
+    return vec;
+}
 
-    double R_b_ccw {};
-    double R_b_cw {};
-    for (size_t i {}; i < pointForceVector_2d.size(); i++)    {                                                          /*Iterates through all forces in 2D vector*/
-        if (pointForceVector_2d.at(i).at(1) < SupportDistances.at(0))    {                                               /*and checks whether they induce a cw or ccw*/
-            R_b_ccw += (SupportDistances.at(0) - pointForceVector_2d.at(i).at(1)) * pointForceVector_2d.at(i).at(0);     /*moment. Both are summed up seperately .*/
-        }
-        else {
-            R_b_cw += (pointForceVector_2d.at(i).at(1) - SupportDistances.at(0)) * pointForceVector_2d.at(i).at(0);
-        }
-    }
-    double R_b {(R_b_ccw - R_b_cw) / -(SupportDistances.at(1) - SupportDistances.at(0))};         /*calculates the reaction force at the second support by summing up the cw and ccw moments*/
-    double pointForceSum {};                                                                      /*and dividing by the distance between the 2 supports*/
-    for (size_t i {}; i < pointForceVector_2d.size(); i++)    {                                      /*Iterates through all forces in 2D vector and sums them up*/
+double computeSum (vector <vector<double>> pointForceVector_2d) {
+        double pointForceSum {};                                                                      
+    for (size_t i {}; i < pointForceVector_2d.size(); i++)    {                                      
         pointForceSum += pointForceVector_2d.at(i).at(0);
     }
-    cout << "Sum of point forces = " << pointForceSum << " Newtons" << endl;                
-    double R_a {pointForceSum - R_b};                                                             /*Presumes beam is in equilibrium and uses sum of point forces and reaction forces */
-    cout << "R_a at " << SupportDistances.at(0) << " metres = " << R_a << " Newtons" << endl;     /*to calculate the reaction at the first support.*/
-    cout << "R_b at " << SupportDistances.at(1) << " metres = " << R_b << " Newtons" << endl;
+    return pointForceSum;
+}
 
+double computeR_b (vector <vector<double>> pointForceVector_2d, vector <double> supportDistances) {
+    double R_b_ccw {};
+    double R_b_cw {};
+    for (size_t i {}; i < pointForceVector_2d.size(); i++)    {                                                          
+        if (pointForceVector_2d.at(i).at(1) < supportDistances.at(0))    {                                               
+            R_b_ccw += (supportDistances.at(0) - pointForceVector_2d.at(i).at(1)) * pointForceVector_2d.at(i).at(0);     
+        }
+        else {
+            R_b_cw += (pointForceVector_2d.at(i).at(1) - supportDistances.at(0)) * pointForceVector_2d.at(i).at(0);
+        }
+    }
+    return ((R_b_ccw - R_b_cw) / - (supportDistances.at(1) - supportDistances.at(0)));
+}
+
+double computeR_a (double R_b, double sumPointForces)   {
+    return (sumPointForces - R_b);
+}
+
+void outputReactions(double sumPointForces, double R_a, double R_b)  {
+    cout << "Sum of Forces = " << sumPointForces << endl;
+    cout << "Ra = " << R_a << endl;
+    cout << "Rb = " << R_b << endl;
     cout << "================================================" << endl;
+}
 
+vector <vector<double>> createForceVector (vector <vector<double>> pointForceVector_2d, vector <vector<double>> UDLVector_2d,
+/*                                         */vector <double> supportDistances, vector <double> vec, double R_a, double R_b)  {
     vector <vector<double>> forceVector {};
     for (int i {}; i < pointForceVector_2d.size(); i++) {
         if (pointForceVector_2d.at(i).at(2) == 0)   {
@@ -117,61 +197,65 @@ int main()  {
         vec.at(2) = UDLVector_2d.at(i).at(2);
         forceVector.push_back(vec);
     }
-
     vec.at(0) = - R_a;
-    vec.at(1) = SupportDistances.at(0);
-    vec.at(2) = 0;                           /*Adds the reaction forces to the 2D vector*/
+    vec.at(1) = supportDistances.at(0);
+    vec.at(2) = 0;                        
     forceVector.push_back(vec);
-
     vec.at(0) = - R_b;
-    vec.at(1) = SupportDistances.at(1);
+    vec.at(1) = supportDistances.at(1);
     vec.at(2) = 0;   
     forceVector.push_back(vec);
+    return forceVector;
+}
 
-    sort(forceVector.begin(), forceVector.end(), SortDistance);
+bool SortDistance (const vector <double>& v1, const vector <double>& v2)    {
+    return v1.at(1) < v2.at(1);
+}
 
+void outputForceVector (vector <vector<double>> forceVector)    {
     for (int i {}; i < forceVector.size(); i++)   {
         cout << forceVector.at(i).at(0) << " Newtons " << forceVector.at(i).at(1) << " Metres";
         if (forceVector.at(i).at(2) != 0)   {
-            cout << " to " << forceVector.at(i).at(2) << " Metres" << endl;
+        cout << " to " << forceVector.at(i).at(2) << " Metres" << endl;
         }
         else    {
-            cout << endl;
+        cout << endl;
         }
     }
-
     cout << "================================================" << endl;
-    double stepSize {beamLength / 100.0,};
-    double Vx {};
-    double Mx {};
-    vector <vector<double>> SFBM_2D {};
-    for (double x {}; x <= beamLength; x += stepSize)   {
-        for (int i {}; i < forceVector.size(); i++)   {
-            if (x >= forceVector.at(i).at(1))   {
-                if (forceVector.at(i).at(2) == 0)    {
-                    Vx += forceVector.at(i).at(0);
-                    Mx += forceVector.at(i).at(0) * (x - forceVector.at(i).at(1));
-                }
-                else if (forceVector.at(i).at(2) <= x)   {
-                    Vx += forceVector.at(i).at(0);
-                    Mx += forceVector.at(i).at(0) * (x - (forceVector.at(i).at(1) + ((forceVector.at(i).at(2) - forceVector.at(i).at(1)) / 2)));
-                }
-                else    {
-                    Vx += forceVector.at(i).at(0) * (x - forceVector.at(i).at(1));
-                    Mx += (forceVector.at(i).at(0) * (x - forceVector.at(i).at(1))) * ((x - forceVector.at(i).at(1)) / 2);
-                }
-            }
-        }
-        vec.at(0) = x;
-        vec.at(1) = Vx;
-        vec.at(2) = Mx;
-        SFBM_2D.push_back(vec);
-        Vx = 0;
-        Mx = 0;
+}
+
+double computeStepSize (double beamLength) {
+    return (beamLength / 100);
+}
+
+double computeVx (vector <vector<double>> forceVector, double x, int i) {
+    if (forceVector.at(i).at(2) == 0)    {
+        return forceVector.at(i).at(0);
     }
+    else if (forceVector.at(i).at(2) <= x)   {
+        return forceVector.at(i).at(0);
+    }
+    else    {
+        return (forceVector.at(i).at(0) * (x - forceVector.at(i).at(1)));
+    }
+}
+
+double computeMx (vector <vector<double>> forceVector, double x, int i) {
+    if (forceVector.at(i).at(2) == 0)    {
+        return (forceVector.at(i).at(0) * (x - forceVector.at(i).at(1)));
+    }
+    else if (forceVector.at(i).at(2) <= x)   {
+        return (forceVector.at(i).at(0) * (x - (forceVector.at(i).at(1) + ((forceVector.at(i).at(2) - forceVector.at(i).at(1)) / 2))));
+    }
+    else    {
+        return ((forceVector.at(i).at(0) * (x - forceVector.at(i).at(1))) * ((x - forceVector.at(i).at(1)) / 2));
+    }
+}
+
+void outputSFBM (vector <vector<double>> SFBM_2D)   {
     for (int i {}; i < SFBM_2D.size(); i++) {
         cout << "Distance (m): " << SFBM_2D.at(i).at(0) << " Shear Force (N): " << SFBM_2D.at(i).at(1) << " Bending Moment (Nm): " << SFBM_2D.at(i).at(2) << endl;
     }
     cout << "================================================" << endl;
-    return 0;
 }
